f06aca4ad5299515cc23eda5028b4b3fab6a59ae
pwlsc21 <94947594+pwlsc21@users.noreply.github.com>
2021-11-23 23:31:27 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBPHK5xeg0EMxOU8X_nL6yyUZWqSs20FL4",
+  authDomain: "pwlsc21-ee5eb.firebaseapp.com",
+  projectId: "pwlsc21-ee5eb",
+  storageBucket: "pwlsc21-ee5eb.appspot.com",
+  messagingSenderId: "636197014200",
+  appId: "1:636197014200:web:2a88ee5eb75b34fec6824d",
+  measurementId: "G-PK439V0F02"
+});
